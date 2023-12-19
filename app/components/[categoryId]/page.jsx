42eca985f33548426eacd2ba93aee14fcb6ce2d7
spandagle <siddhant.pandagle1998@gmail.com>
2023-12-19 11:53:21 +0530

    CategoryID updaetd

--- app/components/[categoryId]/page.jsx
+++ app/components/[categoryId]/page.jsx
@@ -1,11 +1,12 @@
 import { apiURL } from '@/app/constants';
+import ComponentList from '@/components/ComponentList';
 import PageHeading from '@/components/PageHeading'
 import PreviewComponent from '@/components/PreviewComponent';
 import React from 'react'
 
 
 async function getCodesByCategoryId(categoryId) {
-    const res = await fetch(`${apiURL}/code/category/${categoryId}`)
+    const res = await fetch(`${apiURL}/category/${categoryId}`)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
@@ -16,8 +17,10 @@ export default async function Category({ params }) {
     const { categoryId } = params;
     const codeData = await getCodesByCategoryId(categoryId);
     return (
-        <div className="py-8 bg-white">
-            <PreviewComponent codeData={codeData}/>
-        </div>
+      <div className="py-8 bg-white">
+        {/* {JSON.stringify(codeData)} */}
+          <PageHeading heading='Category' subHeading='Free Tailwind CSS Components' content='Working on an CMS, eCommerce platform or admin hub? SynergyUI has a growing range of Tailwind CSS components, that will help you build an accessible, responsive application for your next project.'/>
+          <ComponentList category={codeData.title} />
+      </div>
     )
 }
